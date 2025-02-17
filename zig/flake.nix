95a4c9da37fc9a26a57d66b1b26aed16b2899e5e
sidharta <jeansidharta@gmail.com>
2025-02-17 20:42:10 -0300

    Improve zig template

--- zig/flake.nix
+++ zig/flake.nix
@@ -13,32 +13,34 @@
     utils.lib.eachDefaultSystem (
       system:
       let
+        project_name = "INSERT-PROJECT-NAME-HERE";
+        version = "0.0.1";
+        deps = {
+          # dep = dep;
+        };
+
         pkgs = nixpkgs.legacyPackages.${system};
         zig = zig-flake.outputs.packages.${system}.master;
         mkLibsLinkScript = ''
-          mkdir -p libs
-          rm --force libs/hyprland-zsock
-          # ln -s <YOUR DEPENDENCIES HERE> libs/<DEPENDENCY_NAME>
+          rm --force libs
+          ln -s ${pkgs.linkFarm (project_name + "-deps") deps} libs
         '';
         package = pkgs.stdenv.mkDerivation {
-          name = "workspaces-report";
-          version = "0.0.1";
+          pname = project_name;
+          version = version;
           src = ./.;
           buildInputs = [
             zig
-            pkgs.which
           ];
 
           buildPhase = ''
-            echo "custom build phase"
-            cd $TEMP
             cp --no-preserve=mode $src/* . -r
             ${mkLibsLinkScript}
 
             zig build \
               --prefix $out \
               --release=safe \
-              -Doptimize=Debug \
+              -Doptimize=ReleaseSafe \
               -Ddynamic-linker=$(cat $NIX_BINTOOLS/nix-support/dynamic-linker) \
               --cache-dir cache \
               --global-cache-dir global \
